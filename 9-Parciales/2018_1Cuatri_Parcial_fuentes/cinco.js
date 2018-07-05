bd94a6d2ad6ca9d085d1b84fcf2ec012df087c4f
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-05 01:08:56 -0300

    verificar
    no esta bien, verificar errores.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	 //Nivel1 de parcial, practica.
+//1
+
+ 	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("el ancho es","5");
+	largo=prompt("el largo es","5");
+
+	perimetro=(ancho *2)+(largo*2);
+
+	alert("El perimetro es: ");
+
+
+//2
+
+	//	alert("usted es xxxxx y vive en la localidad")
+
 
 }
