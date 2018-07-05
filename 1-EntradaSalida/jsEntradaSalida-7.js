cd2aa741d8df586e9a7a8dcc1d4d74d7033369c9
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-05 00:42:55 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -5,6 +5,24 @@ ej.: "la Resta es 750"*/
 function sumar()
 {	
 	
+	var	numero1;
+	var	numero2;
+	var resultado;
+
+	numero1=document.getElementById("numeroUno").value;	
+	numero2=document.getElementById("numeroDos").value;
+	
+	numero1=parseInt (numero1);
+	numero2=parseInt (numero2);
+
+	resultado=numero1+numero2;// no me suma los valures, ponia el resultado antes de tranformalos 
+	// a enteros, por lo cual me mostraba. cambie el orden y ahora me los suma.
+	
+
+	alert ("la suma es: "+ resultado);
+
+
+
 }
 
 function restar()
