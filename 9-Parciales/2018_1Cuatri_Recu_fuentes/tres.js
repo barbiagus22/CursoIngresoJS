6f5496f5501b4b7dee80fbeb5d720726dd0d8850
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-05 21:57:58 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var preciofinal;
+
+
+	precio=prompt("Ingrese el precio");
+	descuento=prompt("Ingrese el descuento");
+
+	descuento=precio-descuento;
+	alert("El descuento es: " + descuento);
+
+	precio=precio-descuento;
+	alert("El precio con el descuento: " + precio);
+
+	//document.getElementById('descuento').value=descuento;
+
+
+	//descuento=pasertIn(descuento);
+	//precio= precio-descuento;
+	//preciofinal= (precio*0.21);
+
+	//document.getElementById('elPrecioFinal').value=preciofinal;
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+	var	base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base=prompt("ingrese la base");
+	altura=prompt("ingrese la altura");
+
+	superficie=(base*altura)/2;
+	perimetro=(base*3);
+
+	alert("la superficie es: " +superficie + "el perimetro es: "+perimetro);
+	
+
+
+
+
 
 }
