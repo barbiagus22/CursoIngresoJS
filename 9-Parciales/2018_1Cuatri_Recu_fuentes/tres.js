22b2037bd9f40195959c44d3f3052ddf21ae15d7
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-06 00:17:19 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -3,16 +3,31 @@ function mostrar()
 	var precio;
 	var descuento;
 	var preciofinal;
-
+	var iva;
 
 	precio=prompt("Ingrese el precio");
 	descuento=prompt("Ingrese el descuento");
 
-	descuento=precio-descuento;
+	descuento=(precio*descuento)/100;
 	alert("El descuento es: " + descuento);
 
 	precio=precio-descuento;
 	alert("El precio con el descuento: " + precio);
+
+	iva=(precio*0.21)+(descuento*0.21);
+	alert("El iva es : "+ iva);
+
+	preciofinal=precio+iva;
+
+	document.getElementById('elPrecioFinal').value=preciofinal;
+
+
+
+
+
+
+
+
 
 	//document.getElementById('descuento').value=descuento;
 
