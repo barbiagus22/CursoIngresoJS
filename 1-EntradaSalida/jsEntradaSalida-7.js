cfb5e7c1f5c2c91f10cf57673c10cfe750a35517
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-12 00:51:46 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,54 +4,72 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-	var	numero1;
-	var	numero2;
+	var numeroUno;
+	var numeroDos;
 	var resultado;
 
-	numero1=document.getElementById("numeroUno").value;	
-	numero2=document.getElementById("numeroDos").value;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt (numeroUno);
+
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt (numeroDos);
+
+	resultado=numeroUno+numeroDos;
+
+	alert( "La suma es :  " + resultado);
+
 	
-	numero1=parseInt (numero1);
-	numero2=parseInt (numero2);
-
-	resultado=numero1+numero2;// no me suma los valures, ponia el resultado antes de tranformalos 
-	// a enteros, por lo cual me mostraba. cambie el orden y ahora me los suma.
-	
-
-	alert ("la suma es: "+ resultado);
-
 
 }
 
 function restar()
 {
 	
-	var numero1;
-	var numero2;
+	var numeroUno;
+	var numeroDos;
+	var resta;
 
-	numero1=document.getElementById('numeroUno').value;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=parseInt(numeroUno);
 
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
 
+	resta=numeroUno-numeroDos;
 
-
-
-
-
-
-
-
-
+	alert("La resta es : "+ resta );
 
 }
 
 function multiplicar()
 { 
 	
+	var numeroUno;
+	var numeroDos;
+	var mutiplicacion;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	mutiplicacion=numeroUno*numeroDos;
+	alert("La mutiplicacion es : " + mutiplicacion);
+
+
+
 }
 
 function dividir()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var division;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	division=numeroUno/numeroDos;
+	alert("La division es : " + division);
 }
 
