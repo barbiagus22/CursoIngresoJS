1fbccea57bce8b460a05209ddda208d8c78cbd34
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-18 22:03:02 -0300

    Add files via upload

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,80 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
+
+var	eleccionMaquina;
+
 
 function comenzar()
 {
-	
+	//Genero el número RANDOM entre 1 y 3
+	 	
+	eleccionMaquina= Math.floor((Math.random() * 3) + 1);
+	alert (eleccionMaquina);
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
+	var piedra;
+	piedra= 1
 	
+
+	if (eleccionMaquina== 1 ) {	
+
+			alert(" Empato");
+		}
+		else if (eleccionMaquina == 2) {
+
+			alert (" Perdio");
+		}
+
+		else if ( eleccionMaquina == 3) {
+
+			alert ("Ganó");
+		}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	var papel;
+	papel=2
 
+
+	if (eleccionMaquina == 1) {
+
+		alert ("Ganó");
+		}
+
+		else if (eleccionMaquina ==2) {
+
+			alert ("Empato");
+		}
+		else if (eleccionMaquina==3) {
+
+			alert ("Perdio");
+		}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	var tijera;
+	tijera=3
 
+
+	if (eleccionMaquina == 1) {
+
+		alert ("Perdio");
+		}
+
+		else if ( eleccionMaquina == 2 ) {
+
+			alert (" Ganó");
+		}
+
+		else if ( eleccionMaquina == 3) {
+
+			alert ( "Empato");
+		}		
+	
 }//FIN DE LA FUNCIÓN
