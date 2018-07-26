29c4294c53f6566a20b9f7ce392e8fa71fb740cf
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-26 03:23:30 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,52 @@
+
+	
+
 function mostrar()
 {
+
+	var numeroUno;
+	var	numeroDos;
+	var resta;
+	var suma;
+
+	numeroUno=prompt (" Ingreso un numero");
+	numeroUno=parseInt(numeroUno);
+	
+	numeroDos=prompt ( " Ingrese un segundo numero");
+	numeroDos=parseInt(numeroDos);
+	
+
+
+
+	if (numeroUno==numeroDos) {
+
+		alert ( " El primer numero ingresado " + numeroUno + "es igual al segundo numero ingresado " + numeroDos );
+	}
+
+	else if (numeroUno>numeroDos) {
+
+		resta=numeroUno-numeroDos;
+		alert ( " La resta es  " + resta);
+		
+	}
+
+	else if (numeroUno<numeroDos) {
+
+		suma=numeroUno+numeroDos;
+		alert ( "La suma es " + suma);
+
+		if (suma>10) {
+
+			alert ( "La suma es " + suma + "y supero el 10");
+		}
+	
+	}
+
+
+
+
+
+
+
 
 }
