a77a1fa0281f2b08e9360287b74122d26c7da59a
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-26 03:42:46 -0300

    Add files via upload

---
+++ cuatro.js
@@ -0,0 +1,57 @@
+
+	
+
+function mostrar()
+{
+
+	var numeroUno;
+	var	numeroDos;
+	var resta;
+	var suma;
+
+	numeroUno=prompt (" Ingreso un numero");
+	numeroUno=parseInt(numeroUno);
+	
+	numeroDos=prompt ( " Ingrese un segundo numero");
+	numeroDos=parseInt(numeroDos);
+	
+
+// PRIMERA OPCION IF--------------------------------------------------------------------
+
+/*
+
+	if (numeroUno==numeroDos) {
+
+		alert ( " El primer numero ingresado " + numeroUno + "es igual al segundo numero ingresado " + numeroDos );
+	}
+
+	else if (numeroUno>numeroDos) {
+
+		resta=numeroUno-numeroDos;
+		alert ( " La resta es  " + resta);
+		
+	}
+
+	else if (numeroUno<numeroDos) {
+
+		suma=numeroUno+numeroDos;
+		alert ( "La suma es " + suma);
+
+		if (suma>10) {
+
+			alert ( "La suma es " + suma + "y supero el 10");
+		}
+	
+	}
+
+
+*/
+
+
+// SEGUNDA OPCION SWICH--------------------------------------------------
+
+
+
+
+
+}
