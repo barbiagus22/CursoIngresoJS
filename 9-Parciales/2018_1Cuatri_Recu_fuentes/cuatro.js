498e277242bd9442f87d787c0e0a2e5a0fd39df5
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-07-29 22:45:24 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
 
+	var numeroUno;
+	var numeroDos;
+	var	division;
+	var suma;
+
+	numeroUno=prompt ( "Ingrese un numero");
+	numeroDos=prompt ( "Ingrese otro numero");
+	
+
+
+	if (numeroUno==numeroDos) {
+
+		alert (numeroUno+numeroDos);
+	}
+	else if (numeroUno>numeroDos) {
+		
+		division=numeroUno/numeroDos;
+		numeroUno=parseInt (numeroUno);
+		numeroDos=parseInt (numeroDos);
+		alert ("La division es: " + division);
+	}
+	else if (numeroUno<numeroDos) {
+
+		numeroUno=parseInt (numeroUno);
+		numeroDos=parseInt (numeroDos);
+		suma=numeroUno+numeroDos;
+		alert ( "La suma es :  " + suma );
+	} 	
+	if (suma<50) {
+
+		alert (" la suma es menor a 50");
+	}
+	
 }
