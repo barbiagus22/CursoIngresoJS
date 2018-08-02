288d1405ac4e4098d824f8e9b92bd9891b7bc9bb
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-08-02 00:01:25 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
+/*
+	
+	NOMBRE		SEXO		EDAD
+	jose 		m 			25
+	maria 		f 			16
+	jesus		m 	   	 	33
+	fer 		f 			81
+*/
+
+// Siempre que tenemos cantidad , ejecutar contador. 
+
 function mostrar()
 {
 
-}
+	var contador;
+	var nombre;
+	var sexo;
+	var edad;
+	var cantidaddeMujeres=0;
+	var cantidaddeHombres=0;
+
+
+	while (contador<4) {
+
+		contador=contador+1;
+
+
+		nombre= prompt("Ingrese nombre");
+
+		sexo = prompt ("Ingrese sexo");
+
+		while (sexo!= 'F' && sexo!='M') {
+
+			sexo = prompt ("Ingrese sexo");
+		}
+
+		if (sexo=='F') {
+
+			cantidaddeMujeres=cantidaddeMujeres+1;
+		}
+		else {
+
+			cantidaddeHombres=cantidaddeHombres+1;
+		}
+
+
+	}
